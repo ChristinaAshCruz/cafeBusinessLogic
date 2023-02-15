5d82dea9afc79e86a3066a9d5cd061313a44159a
ChristinaAshCruz <christina.ash.cruz@gmail.com>
2023-02-14 18:41:38 -0800

    final touches + ninja bonuses

--- CafeUtil.java
+++ CafeUtil.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
 
 public class CafeUtil {
-    // reward # 
-    // numOfDrinksPurchased
     private String customerName;
     private int numWeeks;
 
@@ -49,5 +47,53 @@ public class CafeUtil {
             String oneMenuItem = String.format("%s %s", id, menuItems.get(id));
             System.out.println(oneMenuItem);
         } 
+    }
+
+    public void addCustomer(ArrayList<String> customers){
+        System.out.println("Please enter your name");
+        String userName = System.console().readLine();
+        System.out.println("Hello, " + userName + "!");
+        System.out.println("There are " +  customers.size() + "  people in front of you");
+        customers.add(userName);
+
+    }
+
+    // NINJA BONUS
+    public void printPriceChart(String product, double price, int maxQuantity){
+        System.out.println(product + "\n");
+        double costPrice = 0;
+        for (int i = 1; i < maxQuantity; i++){
+            costPrice = costPrice + price;
+            String cost = String.format( "%s - $%.2f", i, costPrice);
+            System.out.println(cost);
+        }
+    }
+    
+    public void bulkDiscount(String product, double price, int maxQuantity){
+        System.out.println(product + "\n");
+        double discount = .5;
+        double costPrice = 0;
+        for (int i = 0; i < maxQuantity; i++){
+            if (i == 0) {
+                costPrice = price + costPrice;
+                Integer id = i + 1;
+                String returnBulkPrice = String.format ("%s - $%.2f", id, costPrice);
+                System.out.println(returnBulkPrice);
+            }
+            if (i <= 3 && i != 0) {
+
+                costPrice = costPrice + (price - discount);
+                Integer id = i + 1;
+                String returnBulkPrice = String.format ("%s - $%.2f", id, costPrice);
+                System.out.println(returnBulkPrice);
+            }
+            if (i > 3  && i != 0){
+                discount = discount + .5;
+                costPrice = costPrice + (price - discount);
+                Integer id = i + 1;
+                String returnBulkPrice = String.format ("%s - $%.2f", id, costPrice);
+                System.out.println(returnBulkPrice);
+            }
+        }
     }
 }
--- TestCafe.java
+++ TestCafe.java
@@ -5,41 +5,55 @@ public class TestCafe {
     public static void main(String[] args) {
         CafeUtil appTest = new CafeUtil("Jane", 10);
 
-    /* 
-      You will need add 1 line to this file to create an instance 
-      of the CafeUtil class. 
-      Hint: it will need to correspond with the variable name used below..
-    */
-    	
         /* ============ App Test Cases ============= */
+
         // Testing getName method 
         System.out.println(appTest.getName());
         
         // Testing setNumWeeks method - ability to change # of weeks
         appTest.setNumWeeks(9);
 
+
+        // STREAK GOAL TEST
         System.out.println("\n----- Streak Goal Test -----");
         System.out.printf("Purchases needed by week %s: %s \n\n", appTest.getNumWeeks(), appTest.getStreakGoal());
     
+        // ORDER TOTAL TEST
         System.out.println("----- Order Total Test-----");
         double[] lineItems = {3.5, 1.5, 4.0, 4.5};
         System.out.printf("Order total: %s \n\n",appTest.getOrderTotal(lineItems));
         
-        System.out.println("----- Display Menu Test-----");
         
+        
+        // DISPLAY MENU TEST
+        System.out.println("----- Display Menu Test-----");
         ArrayList<String> menu = new ArrayList<String>();
         menu.add("drip coffee");
         menu.add("cappuccino");
         menu.add("latte");
         menu.add("mocha");
         appTest.displayMenu(menu);
-    
-        // System.out.println("\n----- Add Customer Test-----");
-        // ArrayList<String> customers = new ArrayList<String>();
-        // // --- Test 4 times ---
-        // for (int i = 0; i < 4; i++) {
-        //     appTest.addCustomer(customers);
-        //     System.out.println("\n");
-        // }
+        
+        ADD CUSTOMER TEST
+        System.out.println("\n----- Add Customer Test-----");
+        ArrayList<String> customers = new ArrayList<String>();
+        for (int i = 0; i < 4; i++) {
+                appTest.addCustomer(customers);
+                System.out.println("\n");
+            }
+            
+        // PRINT PRICE CHART TEST
+        System.out.println("\n ----- Print Price Chart Test----- \n");
+        appTest.printPriceChart("Ube Latte", 4.25, 5);
+        
+        // BULK DISCOUNT TEST
+        System.out.println("\n----- Bulk Discount Test----- \n");
+        appTest.bulkDiscount("Americano", 2.00, 5);
+
+        System.out.println("\n");
+        appTest.bulkDiscount("Ube Latte", 4.25, 8);
+        System.out.println("\n");
+        
+
     }
 }
